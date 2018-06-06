0731ccd5a0d718306b9b2e605853864ab2aa14b7
Smith <Cameron.Smith@dot.ny.gov>
2018-06-06 16:05:07 -0400

    worked on styling of the results panel and program logic

--- Widget.js
+++ Widget.js
@@ -12,10 +12,11 @@ define([
   'esri/geometry/geometryEngine',
   'dojo/dom',
   'dojo/dom-construct',
-  'dojo/on'
+  'dojo/on',
+  'dojo/dom-style'
 ],
   function (declare, BaseWidget, QueryTask, Query, Graphic, GraphicsLayer, SimpleLineSymbol, Color, esriConfig, geoEngine,
-    dom, domConstruct, on) {
+    dom, domConstruct, on, domStyle) {
     return declare([BaseWidget], {
 
       baseClass: 'construction-projects-widget',
@@ -25,9 +26,6 @@ define([
 
       currCountyGraphicsLyr: null, // graphics layer which represents the current county 
 
-      // add additional properties here
-
-      // methods to communication with app container:
       postCreate: function () {
         this.inherited(arguments);
 
@@ -40,34 +38,33 @@ define([
       onReceiveData: function (name, widgetId, data) {
         console.log("OUTPUT:::", name, widgetId, data);
         if (name === "Search" && data["selectResult"]) {
-          console.log("IN IF BRANCH");
-          let countyData = this._getCountyWithin(data.selectResult.result.feature.geometry);
-          countyData.then(result => {
-            console.log(result);
+          let countyDeferred = this._getCountyThatPointIsIn(data.selectResult.result.feature.geometry);
+          countyDeferred.then(result => {
+            // Update Ui
+            this._toggleResultsDisplay("showList");
             let countyGeometry = result.features["0"].geometry;
-            // Assign county name to RESULTS BOX display here
-            this.currentCounty.innerHTML = result.features["0"].attributes.NAME;
+            this.currentCounty.innerHTML = result.features["0"].attributes.NAME; // Display County in UI
             this._outlineCounty(countyGeometry);
-            let projectsDataForCounty = this._getProjectsByCounty(countyGeometry);
-            console.log("on the way to projects for the county");
-            projectsDataForCounty.then(projectsInCounty => {
-              console.log("PROJECTS IN COUNTY", projectsInCounty);
+
+            // Use county geom to find projects
+            let projDataForCountyDeferred = this._getProjectsByCounty(countyGeometry);
+            projDataForCountyDeferred.then(projectsInCounty => {
               this._displayProjects(projectsInCounty.features);
             }, err => {
-              console.log("error,", err);
+              console.log("Error getting Project information: ", err);
             });
 
           }, err => {
-            console.log("error,", err);
+            console.log("Error getting County information: ", err);
           });
         }
       },
 
-      _getCountyWithin: function (geometry) {
+      _getCountyThatPointIsIn: function (pointGeometry) {
         var queryTask = new QueryTask(this.countiesLyr);
 
         var query = new Query();
-        query.geometry = geometry;
+        query.geometry = pointGeometry;
         query.spatialRelationship = Query.SPATIAL_REL_WITHIN;
         query.returnGeometry = true;
 
@@ -87,10 +84,10 @@ define([
         graphicslayer.add(countyGraphic);
         this.currCountyGraphicsLyr = graphicslayer;
         this.map.addLayer(graphicslayer);
-        console.log("graphic added");
+        console.log("outline graphic added");
       },
 
-      _getProjectsByCounty(countyGeometry) {
+      _getProjectsByCounty(countyGeometry) { // REFACTOR to include queries to all Project types, then return All Promises
         var queryTask = new QueryTask(this.constructionProjectsLyr);
 
         var query = new Query();
@@ -100,44 +97,43 @@ define([
         query.where = "1=1";
         query.outFields = ["*"];
 
-        console.log(query);
-
         return queryTask.execute(query);
       },
 
 
-      _displayProjects(projects) {
-        let mainList = domConstruct.create("ul", {
-          className: "resultsList"
-        }, this.resultsDisplay); //Dojo data attach point
-
-        for (let x = 0; x < projects.length; x++) {
-          this._constructResultItem(projects[x], mainList);
+      _toggleResultsDisplay(newCase) {
+        let list = dom.byId("content-list");
+        let defaultMsg = dom.byId("default-message");
+        switch(newCase) {
+          case "showList":
+            domStyle.set(list, "display", "block");
+            domStyle.set(defaultMsg, "display", "none");
         }
       },
 
 
-      _constructResultItem(projectItem, list) {
-        // Zoom and Popup Handled here
-        function handleZoomTo(evt, destPoint) {
-          console.log(this);
+      _displayProjects(projects) {
+        let mainList = domConstruct.create("div", {
+          className: "resultsList"
+        }, this.resultsDisplay); //data dojo attach point
 
-          this.map.infoWindow.hide();
+        let mainColFlex = domConstruct.create("div", {
+          className: "resultsListFlex"
+        }, mainList);
 
-          this.map.centerAt(destPoint).then(function(destPoint){
-            // Simulate click on map to bring up pop-up
-            var centerScreen = this.map.toScreen(destPoint);
-            this.map.onClick({
-              mapPoint: destPoint,
-              screenPoint: centerScreen
-            });
-          }.bind(this, destPoint));
+        for (let x = 0; x < projects.length; x++) {
+          this._constructResultItem(projects[x], mainColFlex);
         }
+      },
 
-        divElem = domConstruct.create("div", {}, list);
 
-        domConstruct.create("li", {
-          className: "listItem",
+      _constructResultItem(projectItem, mainColumnArea) {
+        // Zoom and Popup Handled here
+
+        divElem = domConstruct.create("div", {className: "flexRow resultListFormat"}, mainColumnArea);
+
+        domConstruct.create("p", {
+          className: "resultItemText",
           innerHTML: projectItem.attributes.TITLE
         }, divElem);
 
@@ -146,7 +142,22 @@ define([
           innerHTML: "<img width='14px' height='20px' src='./widgets/ConstructionProjectsWidget/images/zoomTo.png' />"
         }, divElem);
 
-        on(zoomButton, "click", handleZoomTo.bind(this, null, projectItem.geometry));
+        on(zoomButton, "click", this.zoomAndSimulateClick.bind(this, null, projectItem.geometry));
+      },
+
+
+      zoomAndSimulateClick(evt, destPoint) {
+
+        this.map.infoWindow.hide();
+
+        this.map.centerAt(destPoint).then(function(destPoint){
+          // Simulate click on map to bring up pop-up
+          var centerOfScreen = this.map.toScreen(destPoint);
+          this.map.onClick({
+            mapPoint: destPoint,
+            screenPoint: centerOfScreen
+          });
+        }.bind(this, destPoint));
       }
 
 
